5ff5d6795bafd82fe64db0ec8a98c10dc818fc88
Nolano-Smurphy <56742306+Nolano-Smurphy@users.noreply.github.com>
2024-02-09 12:38:14 -0700

    Fleshed out the toppings overview page based on the style template and by pulling the toppings options from the database.

--- PizzaManager/views.py
+++ PizzaManager/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Topping, Pizza
+
 # Create your views here.
 def index(request):
     return HttpResponse(render(request, "PizzaManager/landing_page.html"))
 
 def toppings_overview(request):
-    return HttpResponse("<h1>Hello World! This is the Toppings Overview!</h1>")
+    toppings_list = Topping.objects.order_by("name")[:5]
+    context = {"toppings_list": toppings_list}
+    return HttpResponse(render(request, "PizzaManager/toppings_overview.html", context))
 
 def toppings_editor(request, topping_id):
     final_response = "<h1>Hello World! This is the Toppings Editor!</h1><br>"
