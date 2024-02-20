4195eadc1e02ff87f0c868e0b4e6f6b27c8338bf
Nolano-Smurphy <56742306+Nolano-Smurphy@users.noreply.github.com>
2024-02-19 20:27:41 -0700

    Added comments to make the tests easier to read.

--- PizzaManager/tests/test_toppings.py
+++ PizzaManager/tests/test_toppings.py
@@ -23,12 +23,24 @@ class TestToppings(TestCase):
         self.client = Client()
         return super().setUp()
 
+    ##############################################################################
+    ###         It should allow me to see a list of available toppings         ###
+    ##############################################################################
+
     def test_list_available_toppings_empty(self):
+        """
+        This test will ensure the client receives an empty list if nothing is in the database when
+        the overview menu is requested.
+        """
         response = self.client.get(reverse("PizzaManager:Toppings Overview"))
         self.assertEqual(response.status_code, HTTP_OK)
         self.assertQuerySetEqual(response.context["toppings_list"], [])
 
     def test_list_available_toppings_contents(self):
+        """
+        This test will ensure the client receives a filled list if something is in the database when
+        the overview menu is requested.
+        """
         self.client.post("/toppings/new", { 
             "new_topping_name": "TestTopping",
             "topping_new": "topping_new"
@@ -38,17 +50,38 @@ class TestToppings(TestCase):
         self.assertEqual(response.status_code, HTTP_OK)
         self.assertQuerySetEqual(response.context["toppings_list"], [topping])
 
+    ##############################################################################
+    ###                It should allow me to add a new topping                 ###
+    ##############################################################################
+
     def test_add_topping(self):
+        """
+        This test will ensure the server correctly redirects the client and returns a
+        filled toppings list when going back to the Topping Overview after requesting a topping addition.
+        """
         getPageResponse = self.client.get("/toppings/new")
         self.assertEqual(getPageResponse.status_code, HTTP_OK)
         toppingPostResponse = self.client.post("/toppings/new", { 
             "new_topping_name": "TestTopping",
             "topping_new": "topping_new"
         })
+        topping = Topping.objects.get(name="TestTopping")
+
         self.assertEqual(toppingPostResponse.status_code, HTTP_REDIRECT)
         self.assertRedirects(toppingPostResponse, reverse("PizzaManager:Toppings Overview"))
+        addToppingResponse = self.client.get(reverse("PizzaManager:Toppings Overview"))
+        self.assertEqual(addToppingResponse.status_code, HTTP_OK)
+        self.assertQuerySetEqual(addToppingResponse.context["toppings_list"], [topping])
+
+    ##############################################################################
+    ###            It should allow me to delete an existing topping            ###
+    ##############################################################################
 
     def test_delete_topping(self):
+        """
+        This test will ensure the server correctly redirects the client and returns a
+        filled toppings list when going back to the Topping Overview after requesting a topping deletion.
+        """
         #Ensure adding a topping is successful.
         self.client.post("/toppings/new", { 
             "new_topping_name": "TestTopping",
@@ -66,7 +99,16 @@ class TestToppings(TestCase):
         self.assertEqual(deleteToppingResponse.status_code, HTTP_OK)
         self.assertQuerySetEqual(deleteToppingResponse.context["toppings_list"], [])
 
+    
+    ##############################################################################
+    ###           It should allow me to update an existing topping             ###
+    ##############################################################################
+
     def test_update_topping(self):
+        """
+        This test will ensure the server correctly redirects the client and returns a
+        filled toppings list when going back to the Topping Overview after requesting a topping rename.
+        """
         #Ensure adding a topping is successful.
         self.client.post("/toppings/new", { 
             "new_topping_name": "TestTopping",
@@ -88,7 +130,15 @@ class TestToppings(TestCase):
         self.assertEqual(updateToppingResponse.status_code, HTTP_OK)
         self.assertQuerySetEqual(updateToppingResponse.context["toppings_list"], [topping])
 
+    ##############################################################################
+    ###          It should not allow me to enter duplicate toppings            ###
+    ##############################################################################
+
     def test_no_created_duplicate_toppings(self):
+        """
+        This test will ensure the server keeps the client on the Topping Create page and returns an
+        error message when they enter a name for a new topping when a topping with that name already exists.
+        """
         #Ensure adding first topping is successful.
         firstToppingResponse = self.client.post("/toppings/new", { 
             "new_topping_name": "TestTopping",
@@ -110,6 +160,11 @@ class TestToppings(TestCase):
                          "A topping with this name already exists. Please enter a unique name. No topping created.")
         
     def test_no_updated_duplicate_toppings(self):
+        """
+        This test will ensure the server keeps the client on the Topping Editor page and returns an
+        error message when they enter a name for an existing topping when a topping with that name already exists.
+        """
+
         #Ensure adding first topping is successful.
         firstToppingResponse = self.client.post("/toppings/new", { 
             "new_topping_name": "TestTopping",
